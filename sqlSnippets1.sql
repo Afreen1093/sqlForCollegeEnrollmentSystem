7b0ca24efe248b9358ab355b73a213ef79c03ac2
zakirasim <118193382+zakirasim@users.noreply.github.com>
2023-10-05 10:55:38 -0400

    Update sqlSnippets1.sql

--- sqlSnippets1.sql
+++ sqlSnippets1.sql
@@ -1,3 +1,5 @@
 select * from students;
 insert into students( studentname , programid) values (' Mary' ,  'MAD');
+nsert into students( studentname , programid) values (' John' ,  'CSAT');
+insert into students( studentname , programid) values (' Susan' ,  'CPCT');
 select * from students;
